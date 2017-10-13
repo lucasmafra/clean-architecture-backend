1f7c8f5b2440498aaa1b2dfede6ce978749e2048
Lucas Mafra <lucas.mafra.barros@usp.br>
2017-10-13 17:51:51 -0300

    request parser and response builder

--- src/presentation/graphql-endpoint.ts
+++ src/presentation/graphql-endpoint.ts
@@ -1,13 +1,12 @@
 import { APIGatewayEvent, Callback, Context  } from 'aws-lambda'
 import { graphql} from 'graphql'
 import { MyTopShopSchema } from './graphql'
-import { MyTopShopRequest } from './my-top-shop-request'
-import { responseBuilder } from './response-builder'
+import { requestParser } from './my-top-shop-request'
+import { responseBuilder } from './my-top-shop-response'
 
 export async function endpoint(event: APIGatewayEvent, context: Context, callback: Callback) {
     try {
-        const body = JSON.parse(event.body as string)
-        const request = new MyTopShopRequest(body.query, body.variables, event.headers['Identity-Provider'] || event.headers['identity-provider'] , event.headers.Token || event.headers.token)
+        const request = requestParser(event)
         const result = await graphql(MyTopShopSchema, request.body.query, null, null, request.body.variables)
         if (result.errors) {
             callback(undefined, responseBuilder(500, result))
--- src/presentation/my-top-shop-request.ts
+++ src/presentation/my-top-shop-request.ts
@@ -1,6 +1,18 @@
+import { APIGatewayEvent  } from 'aws-lambda'
+
+export interface IMyTopShopRequestHeaders {
+    identityProvider: string
+    token: string
+}
+
+export interface IMyTopShopRequestBody {
+    query: string
+    variables: { [key: string]: any }
+}
+
 export class MyTopShopRequest {
-    public body: IMyTopShopBody
-    public headers: IMyTopShopHeaders
+    public body: IMyTopShopRequestBody
+    public headers: IMyTopShopRequestHeaders
     constructor(query: string, variables: { [key: string]: any }, identityProvider: string, token: string) {
         this.body = {
             query,
@@ -13,12 +25,12 @@ export class MyTopShopRequest {
     }
 }
 
-export interface IMyTopShopHeaders {
-    identityProvider: string
-    token: string
-}
-
-export interface IMyTopShopBody {
-    query: string
-    variables: { [key: string]: any }
+export function requestParser(event: APIGatewayEvent) {
+    const body = JSON.parse(event.body as string)
+    return new MyTopShopRequest(
+        body.query,
+        body.variables,
+        event.headers['Identity-Provider'] || event.headers['identity-provider'] ,
+        event.headers.Token || event.headers.token,
+    )
 }
